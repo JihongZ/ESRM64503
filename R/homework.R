649e8ab3c256702d9929785211de7676f1a2a8f7
jihong <jzhang@uark.edu>
2024-08-20 11:01:37 -0500

    add homework()

---
+++ R/homework.R
@@ -0,0 +1,16 @@
+#' A Function to check homework of ESRM 64504
+#'
+#' This function allows you to check the progress of homework
+#' @keywords homework
+#' @export
+#' @examples
+#' homework()
+homework <- function(){
+message("
+        There will be four homeworks in total for ESRM 64504.
+        You can work on HW0 now, please click the following link to have access:
+        https://jihongzhang.org/posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/HWs/HW_demo.html
+        Next homework (Homework 1) will be posted on 09/09/2024.
+        For more details, please refer to
+        https://jihongzhang.org/posts/Lectures/2024-07-21-applied-multivariate-statistics-esrm64503/")
+}
